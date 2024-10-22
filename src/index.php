720ab5e41b39e66972de6fbd8500e1048d84534d
SamadhanNirali <samadhan.nirali@gmail.com>
2024-10-22 18:00:51 +0530

    phr

--- src/index.php
+++ src/index.php
@@ -1,12 +1,12 @@
 <?php
 
-echo "welcome ZXXxx";
-$a_variable = 10;
-echo $a_variable;
-
-addition_of_two_number(10,20);
-
 function addition_of_two_number($a,$b)
 {
     return $a+$b;
 }
+
+echo "welcome ZXXxx";
+$a_variable = 10;
+echo $a_variable;
+
+echo addition_of_two_number(10,20);
