a37321f30149689a437c141e0a42150489989552
SamadhanNirali <samadhan.nirali@gmail.com>
2024-10-22 19:23:44 +0530

    coding s

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 require_once('./common_functions.php');
 
 echo "welcome ZXXxx";
-$a_variable = 10;
-echo $a_variable;
+$firstVariable = 10;
+echo $firstVariable;
 
 echo addition_of_two_number(10, 20);
