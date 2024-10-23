62a2aea68378a010ab5076851046fccf3b1d45e2
SamadhanNirali <samadhan.nirali@gmail.com>
2024-10-23 18:03:05 +0530

    adding new phpcs configuration rules

--- src/Test.php
+++ src/Test.php
@@ -1,13 +1,29 @@
 <?php
 
-namespace Vendor\Model;
+/**
+ * Verifies that classes are instantiated with parentheses.
+ *
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @copyright 2006-2015 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ */
+
+     namespace Vendor\Model;
 
 class Test
 {
     protected const PHP_VERSION = 5;
 
-    protected function phpVersion()
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return int
+    */
+
+
+    protected function getPhpVersion()
     {
         return self::PHP_VERSION;
-    }
+    } // end getPhpVersion()
+
 }
